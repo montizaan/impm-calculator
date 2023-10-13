40471b492d515ef7ba50e675068246ae1694e75e
Melvin Montizaan <m.montizaan@monticonsul.nl>
2023-10-13 17:48:38 +0200

    Renewed the calculator programme

--- calculator.py
+++ calculator.py
@@ -1,59 +1,97 @@
-# Initial variable @TODO
-num = 0
+# The welcome message displayed at the beginning of the program
+welcome_message = ("Welcome to Melvin's calculator. \n\n Options: \n Enter '+' for addition. \n Enter '-' for "
+                   "subtraction. \n Enter '*' for multiplication. \n Enter '/' for division. \n Enter 'exit' to end "
+                   "the program.")
 
-# Sum @TODO
-x = 0
-
+# Mathematical operators
 operators = ('+', '-', '*', '/')
 
-
-# Function to perform addition
-def add(num, x):
-    return num + x
+# Results history
+history = [0]
 
 
-# Function to perform subtraction
-def subtract(num, x):
-    return num - x
+# Function of adding two numbers
+def addition(number, operator):
+    return number + operator
 
 
-# Function to perform multiplication
-def multiply(num, x):
-    return num * x
+# Function of subtracting two numbers
+def subtraction(number, operand):
+    return number - operand
 
 
-# Function to perform division
-def divide(num, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return num / y
+# Function of multiplication two numbers
+def multiplication(number, operand):
+    return number * operand
 
 
-while True:
-    # Display menu
-    print("Options:")
-    print("Enter '+' for addition")
-    print("Enter '-' for subtraction")
-    print("Enter '*' for multiplication")
-    print("Enter '/' for division")
-    print("Enter 'exit' to end the program")
+# Function of dividing two numbers
+def division(number, operand):
+    # The number cannot be plugged in, as dividing by zero is not possible, thus raising ValueError
+    if operand == 0:
+        raise ValueError
+    return number / operand
 
-    # Get user input
-    user_input = input(": ")
 
-    if user_input == 'exit':
-        break
+# The calculator function
+def calculator():
+    # Initial number
+    number = 0
 
-    if user_input in operators:
-        x = float(input("Enter number: "))
+    # Result of the equation
+    result = 0
 
-        if user_input == '+':
-            print("Result: ", add(num, x))
-        elif user_input == '':
-            print("Result: ", subtract(num, x))
-        elif user_input == '*':
-            print("Result: ", multiply(num, x))
-        elif user_input == '/':
-            print("Result: ", divide(num, x))
-    else:
-        print("Invalid input. Please try again.")
+    while True:
+        # User inputs the operator with the operand
+        user_input = input("Enter the operator followed by the operand, e.g. + 5: ")
+
+        # The user can query their history with 'q
+        if user_input == 'q':
+            print(history)
+            break
+
+        # Exiting the program
+        if user_input == 'exit':
+            print("Bye!")
+            break
+
+        # Calculator sequence
+        try:
+            # Split the user input in an operator value (str) and operand value (number)
+            operator, operand = user_input.split()
+
+            # Convert the operand to a float variable for the equation
+            operand = float(operand)
+
+            # Check if the first character is an operator
+            if operator in operators:
+                if operator == '+':
+                    result = addition(number, operand)
+                    print(result)
+                elif operator == '-':
+                    result = subtraction(number, operand)
+                    print(result)
+                elif operator == '*':
+                    result = multiplication(number, operand)
+                    print(result)
+                elif operator == '/':
+                    result = division(number, operand)
+                    print(result)
+                else:
+                    raise ValueError
+
+            # The user adds the result to the history list
+            history.append(result)
+
+            # The result of the previous equation becomes the initial value of the new equation
+            number = result
+
+        # The user cannot enter anything other than the operator and operand
+        except ValueError:
+            print("The input provided is not valid. Please check the instructions.")
+
+
+# Instantiate a calculator instance
+if __name__ == "__main__":
+    print(welcome_message)
+    calculator()
